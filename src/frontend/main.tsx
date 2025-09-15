0320cfdb3e52c3eb561570385c027810d1fcdf38
Jocelyn Lin <jocelin.cs@gmail.com>
2025-09-14 18:48:44 -0700

    add css lib (tailwindcss)

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import './index.css';
 
 const rootElem = document.getElementById('main');
 if (!rootElem) {
 	throw new Error('Root element not found');
 }
 const root = createRoot(rootElem);
-root.render(<App name="Hello World" />);
+root.render(<App name="File Explorer" />);
