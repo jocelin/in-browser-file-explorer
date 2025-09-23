447b4aa953f25c63d6c0a622c43edba93b6b454c
Jocelyn Lin <jocelin.cs@gmail.com>
2025-09-22 23:40:39 -0700

    use relative path for builds

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: './',
 	plugins: [react(), tailwindcss()],
 	root: '.',
 	publicDir: 'public',
